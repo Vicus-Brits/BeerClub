028fd7929d13d4ccfc45bf0678ebf0c81a49ecb3
Vicus-Brits <59557634+Vicus-Brits@users.noreply.github.com>
2020-08-04 04:53:50 +0200

    Responsive JavaScript Header

---
+++ img/scripts/main.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    window.alert("sometext");
+    // var y = document.getElementById("toggle_navbar_id");
+    // if (y.className === "navbar_ul") {
+    //     y.className += " navbar_hide";
+    // } else {
+    //     y.className = "navbar_ul";
+    }
