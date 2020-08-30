5c38eae3cf3c735be650dfe50370bed8dcfbd1ce
Vicus-Brits <59557634+Vicus-Brits@users.noreply.github.com>
2020-08-30 11:33:17 +0200

    Learn Sub Buttons working

--- scripts/main.js
+++ scripts/main.js
@@ -9,3 +9,19 @@ function myFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+
+function aboutBeer() {
+    alert("About Beer");
+
+}
+
+function aboutBrews() {
+    alert("About Brews");
+
+}
+
+function aboutBoth() {
+    alert("About Both");
+
+}
